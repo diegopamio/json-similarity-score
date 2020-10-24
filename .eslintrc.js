8990f6ec9c0646c4921b48770d57ed5df4368ca3
Diego Pamio <diego.pamio@gmail.com>
2020-10-24 12:07:25 -0300

    fix: implement workarounds for eslint-babel parser bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,13 @@ module.exports = {
     'plugin:cypress/recommended',
     'airbnb',
   ],
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   plugins: [
     'react',
     'react-hooks',
     'cypress',
     'jest',
+    '@babel',
   ],
   settings: {
     'import/resolver': {
@@ -33,5 +34,8 @@ module.exports = {
     'max-len': ['error', { code: 120 }], // personal preference,to be worthy of seeing my code, you need a large monitor
     'import/prefer-default-export': 'off',
     'react/jsx-filename-extension': [0],
+    indent: ['error', 2, { ignoredNodes: ['TemplateLiteral'] }], // babel-eslint error
+    'template-curly-spacing': 'off', // babel-eslint error
+    'arrow-parens': ['error', 'always'], // codeclimate has an old version of airnbnb-base
   },
 };
