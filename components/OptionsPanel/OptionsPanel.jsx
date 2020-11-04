57afb1c2678694d1a67eee2de1b9134955f7f149
Diego Pamio <diego.pamio@gmail.com>
2020-11-04 18:26:21 -0300

    refactor: remove superfluous custom components
    As detected by code-climate, ArrayCompareOptionPanel.jsx and ElementWeightOptionPanel.jsx could be easily rendered by the same component (OptionsPanel) that was already there, just using a map over each ones' properties.

--- components/OptionsPanel/OptionsPanel.jsx
+++ components/OptionsPanel/OptionsPanel.jsx
@@ -6,8 +6,8 @@ import SwipeableViews from 'react-swipeable-views';
 import { Grid, useTheme } from '@material-ui/core';
 import * as PropTypes from 'prop-types';
 import { CollapsiblePanel } from '~/components/CollapsiblePanel';
-import { ArrayCompareOptionPanel } from '~/components/OptionsPanel/ArrayCompareOptionPanel';
-import { ElementWeightOptionPanel } from '~/components/OptionsPanel/ElementWeightOptionPanel';
+import { SCORE_SETTINGS_VALUES, SCORING_SETTINGS_KEYS } from '~/utils/score/constants';
+import { OptionsTabPanel } from '~/components/OptionsPanel/OptionsTabPanel';
 
 const useStyles = makeStyles({
   root: {
@@ -42,17 +42,53 @@ OptionsTabs.propTypes = {
   onChange: PropTypes.func.isRequired,
 };
 
-const TabPanels = ({ index, onChangeIndex }) => {
+const { ELEMENT_WEIGHT, ARRAY_POSITION_MATCH } = SCORING_SETTINGS_KEYS;
+const { SIBLINGS_PROPORTION, DESCENDANTS_COUNT } = SCORE_SETTINGS_VALUES[ELEMENT_WEIGHT];
+const { FORCE_SAME_POSITION, MATCH_ANY_POSITION } = SCORE_SETTINGS_VALUES[ARRAY_POSITION_MATCH];
+
+const options = [
+  {
+    key: ARRAY_POSITION_MATCH,
+    explanation: 'If two JSONs has only two keys, and one of them is in both JSON a single number, let\'s say `3`,'
+      + ' but in the other key, they both have a four-level nested json with thousands of arrays, sub-objects, '
+      + 'sub-arrays, text and numbers, for which they are only completely different, would you say they are 50% equal'
+      + ' (1 out of 2 keys) or almost completely different? This setting allows you to control exactly that.',
+    alternatives: [
+      { label: 'Disregard Array Index', value: MATCH_ANY_POSITION },
+      { label: 'Force equal Index', value: FORCE_SAME_POSITION },
+    ],
+  },
+  {
+    key: ELEMENT_WEIGHT,
+    explanation: 'This setting determines whether arrays inside the JSON files are compared following their index,'
+    + ' or if they are all compare to each other to find the best match, considering arrays are non-sorted by'
+    + ' nature and thus, they may be considered EQUAL when their equal elements are positioned at different'
+    + ' indexes.',
+    alternatives: [
+      { label: 'Descendants Count', value: DESCENDANTS_COUNT },
+      { label: 'Siblings Proportion', value: SIBLINGS_PROPORTION },
+    ],
+  },
+];
+
+const TabPanels = ({ index: panelIndex, onChangeIndex }) => {
   const theme = useTheme();
 
   return (
     <SwipeableViews
       axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-      index={index}
+      index={panelIndex}
       onChangeIndex={onChangeIndex}
     >
-      <ArrayCompareOptionPanel value={index} index={0} />
-      <ElementWeightOptionPanel value={index} index={1} />
+      {options.map((option, optionIndex) => (
+        <OptionsTabPanel
+          value={panelIndex}
+          index={optionIndex}
+          explanation={option.explanation}
+          alternatives={option.alternatives}
+          optionKey={option.key}
+        />
+      ))}
     </SwipeableViews>
   );
 };
