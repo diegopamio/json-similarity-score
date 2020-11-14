771a6d5357797bbe3ed7ac81a38f6f9d298a7232
Diego Pamio <diego.pamio@gmail.com>
2020-11-14 20:35:23 -0300

    chore: add GTM (to later add GA)

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,21 @@ const CommonHead = () => (
       name="description"
       content="A simple tool for developers to compare json files, with detailed analysis and overall score."
     />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+                (function(w, d, s, l, i){
+                  w[l] = w[l] || []; w[l].push({
+                      'gtm.start':
+                          new Date().getTime(), event: 'gtm.js'
+                  }); var f = d.getElementsByTagName(s)[0],
+                      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+                }) (window, document, 'script', 'dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
+            `,
+      }}
+    />
+
     <script dangerouslySetInnerHTML={{
       __html: `window.heap=window.heap||[],heap.load=function(e,t) {
                   window.heap.appid=e,window.heap.config=t=t||{};
